3a83f7a739a6bb941b9230150d2c54be356a9e06
cuiko <cuiko.tang@gmail.com>
2024-01-09 00:20:53 +0800

    fix(keymap): K do not work

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,8 +3,15 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- stylua: ignore
-      keys[#keys + 1] = { "K", function() require("ufo").peekFoldedLinesUnderCursor() end }
+      keys[#keys + 1] = {
+        "K",
+        function()
+          local winid = require("ufo").peekFoldedLinesUnderCursor()
+          if not winid then
+            vim.lsp.buf.hover()
+          end
+        end,
+      }
     end,
   },
 }
