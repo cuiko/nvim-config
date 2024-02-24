5b049ca00ab9ab163183c36f6507562137ff166e
cuiko <cuiko.tang@gmail.com>
2024-02-24 11:59:09 +0800

    chore: add dependencies

--- lua/plugins/ui/actions-preview.lua
+++ lua/plugins/ui/actions-preview.lua
@@ -1,6 +1,9 @@
 return {
   "aznhe21/actions-preview.nvim",
   event = "VeryLazy",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
   init = function()
     local keys = require("lazyvim.plugins.lsp.keymaps").get()
     -- overlay default keymaps
@@ -27,5 +30,19 @@ return {
       has = "codeAction",
     }
   end,
-  opts = {},
+  opts = {
+    telescope = {
+      sorting_strategy = "ascending",
+      layout_strategy = "vertical",
+      layout_config = {
+        width = 0.8,
+        height = 0.9,
+        prompt_position = "top",
+        preview_cutoff = 20,
+        preview_height = function(_, _, max_lines)
+          return max_lines - 15
+        end,
+      },
+    },
+  },
 }
