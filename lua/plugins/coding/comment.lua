b0fb016b75b5db23ba9d0292f1b3c6a32615789e
cuiko <cuiko.tang@gmail.com>
2024-03-01 03:03:07 +0800

    feat: support mysql comment

--- lua/plugins/coding/comment.lua
+++ lua/plugins/coding/comment.lua
@@ -5,9 +5,16 @@ return {
   },
   {
     "numToStr/Comment.nvim",
-    opts = {
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    },
+    config = function()
+      local ft = require("Comment.ft")
+
+      ft.set("mysql", "--%s")
+      ft.mysql = "--%s"
+
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
     lazy = false,
   },
 }
