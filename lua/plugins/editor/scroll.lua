5755006fdc50ca93d2de37f803c833a56ff97c7d
cuiko <cuiko.tang@gmail.com>
2024-03-01 03:03:28 +0800

    chore: delete unused plugin

--- lua/plugins/editor/scroll.lua
+++ lua/plugins/editor/scroll.lua
@@ -42,39 +42,4 @@ return {
       }
     end,
   },
-  {
-    "petertriho/nvim-scrollbar",
-    enabled = false,
-    opts = {
-      show = true,
-      show_in_active_only = false,
-      set_highlights = true,
-      folds = 1000, -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
-      max_lines = false, -- disables if no. of lines in buffer exceeds this
-      hide_if_all_visible = false, -- Hides everything if all lines are visible
-      throttle_ms = 100,
-      excluded_buftypes = {
-        "terminal",
-      },
-
-      excluded_filetypes = {
-        "neo-tree",
-        "dashboard",
-        "cmp_docs",
-        "cmp_menu",
-        "noice",
-        "prompt",
-        "TelescopePrompt",
-      },
-
-      handlers = {
-        cursor = false,
-        diagnostic = true,
-        gitsigns = true, -- Requires gitsigns
-        handle = true,
-        search = false, -- Requires hlslens
-        ale = false, -- Requires ALE
-      },
-    },
-  },
 }
