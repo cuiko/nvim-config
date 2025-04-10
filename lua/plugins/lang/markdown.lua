6368e52167ecddb3afeb6ed27a2f49876475550f
cuiko <cuiko.tang@gmail.com>
2025-04-10 15:26:58 +0800

    chore: use markdownlint-cli2 instead of markdownlint-cli

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -1,7 +1,6 @@
 return {
+  -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/lang/markdown.lua
   {
-    -- config in LazyExtras
-    -- https://github.com/LazyVim/LazyVim/blob/ec5981dfb1222c3bf246d9bcaa713d5cfa486fbd/lua/lazyvim/plugins/extras/lang/markdown.lua
     "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       code = {
@@ -12,18 +11,10 @@ return {
   {
     "mfussenegger/nvim-lint",
     optional = true,
-    event = "LazyFile",
     opts = {
-      linters_by_ft = {
-        markdown = { "markdownlint" },
-      },
       linters = {
-        markdownlint = {
-          -- stylua: ignore
-          args = {
-            "--ignore", vim.fn.stdpath("data") .. "/gp/chats/*.md",
-            "--disable", "MD013", "MD001", "--",
-          },
+        ["markdownlint-cli2"] = {
+          args = { "--config", os.getenv("XDG_CONFIG_HOME") .. "/nvim/.markdownlint-cli2.yaml", "--" },
         },
       },
     },
